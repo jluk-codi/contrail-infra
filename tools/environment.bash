f9fbdc16677a417786c9425a35fa68c641ec4c81
Your Name <you@example.com>
2017-11-11 21:18:03 +0000

    dynamic current branch resolution

--- tools/environment.bash
+++ tools/environment.bash
@@ -5,4 +5,4 @@ declare -A HOSTS
 HOSTS[puppetdb]=puppetdb2.opencontrail.org
 HOSTS[puppetmaster]=ci-puppetmaster2.opencontrail.org
 ENVIRONMENT=development
-CURRENT_BRANCH=development
+CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
