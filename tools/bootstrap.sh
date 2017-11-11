46c8e6e512bc06bc4cf82ababd1106a80a5b1996
Your Name <you@example.com>
2017-11-11 17:29:55 +0000

    docker bootstrap fixes

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -6,15 +6,15 @@ if [ x"$1" == x ] && [ $EUID -ne 0 -o -z "$SUDO_USER" ]; then
     exit 1
 fi
 
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+PROJECT_DIR=$(readlink -f $SCRIPT_DIR/../)
+MODULES_DIR=${PROJECT_DIR}/modules/
+
+cd "$SCRIPT_DIR"
+. environment.bash
+
 if [ x"$1" == x"master" ] || [ x"$1" == x ]; then
-    SCRIPT_DIR=$(dirname $(readlink -f $0))
-    PROJECT_DIR=$(readlink -f $SCRIPT_DIR/../)
-    MODULES_DIR=${PROJECT_DIR}/modules/
-
-    cd "$SCRIPT_DIR"
-    . environment.bash
-
-
+    apt-get install -y sudo
     if [ ! -x /usr/bin/puppet ]; then
         bash ${SCRIPT_DIR}/install_puppet.sh
     fi
@@ -67,7 +67,7 @@ if [ x"$1" == x"host" ]; then
 puppetdb ansible_host=${HOSTS[puppetdb]} ansible_connection=docker
 puppetmaster ansible_host=${HOSTS[puppetmaster]} ansible_connection=docker
 EOF
-    ansible-playbook ${PROJECT_DIR}/playbooks/docker_puppetmaster_pre.yaml
+    ansible-playbook -vv ${PROJECT_DIR}/playbooks/docker_puppetmaster_pre.yaml
 else
     if [ x"$1" == x ]; then
         # create /etc/ansible/hosts for bootstrap
@@ -77,11 +77,13 @@ puppetdb ansible_host=${HOSTS[puppetdb]}
 puppetmaster ansible_host=${HOSTS[puppetmaster]} ansible_connection=local
 EOF
     fi
+    playbook_sudo="sudo -u ${SUDO_USER}"
 fi
 if [ x"$1" == x"host" ] || [ x"$1" == x ]; then
+    cd "$PROJECT_DIR"
     # install required ansible roles
     ansible-galaxy install -r roles.yaml --force
     # run ansible to finish the bootstrap process
-    rm -rf $HOME/.ansible/
-    sudo -u ${SUDO_USER} ANSIBLE_SSH_PIPELINING=True ansible-playbook ${PROJECT_DIR}/playbooks/bootstrap_puppet.yaml --user ubuntu --sudo --private-key $HOME/.ssh/id_rsa --extra-vars "puppet_environment=$ENVIRONMENT puppetdb_host=${HOSTS[puppetdb]} puppetmaster_host=${HOSTS[puppetmaster]} current_branch=$CURRENT_BRANCH"
+    #rm -rf $HOME/.ansible/
+    $playbook_sudo ANSIBLE_SSH_PIPELINING=True ansible-playbook ${PROJECT_DIR}/playbooks/bootstrap_puppet.yaml --user ubuntu --sudo --private-key $HOME/.ssh/id_rsa --extra-vars "puppet_environment=$ENVIRONMENT puppetdb_host=${HOSTS[puppetdb]} puppetmaster_host=${HOSTS[puppetmaster]} current_branch=$CURRENT_BRANCH"
 fi
